b4da0b9cebb0763115927178d7c25d09ba8db963
Siract <57831221+Ciractt@users.noreply.github.com>
2026-08-08 15:16:23 +0100

    Update fonts.ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -12,20 +12,20 @@ export const gtStandard = localFont({
   variable: '--font-gt-standard',
   display: 'swap',
   src: [
-    { path: './fonts/GT-America-Standard-Regular.woff2', weight: '400', style: 'normal' },
-    { path: './fonts/GT-America-Standard-Regular-Italic.woff2', weight: '400', style: 'italic' },
-    { path: './fonts/GT-America-Standard-Bold.woff2', weight: '700', style: 'normal' },
+    { path: './fonts/GT-America-Standard-Regular.ttf', weight: '400', style: 'normal' },
+    { path: './fonts/GT-America-Standard-Regular-Italic.ttf', weight: '400', style: 'italic' },
+    { path: './fonts/GT-America-Standard-Bold.ttf', weight: '700', style: 'normal' },
   ],
 });
 
 export const gtCondensed = localFont({
   variable: '--font-gt-condensed',
   display: 'swap',
-  src: [{ path: './fonts/GT-America-Condensed-Bold.woff2', weight: '700', style: 'normal' }],
+  src: [{ path: './fonts/GT-America-Condensed-Bold.ttf', weight: '700', style: 'normal' }],
 });
 
 export const gtExpanded = localFont({
   variable: '--font-gt-expanded',
   display: 'swap',
-  src: [{ path: './fonts/GT-America-Expanded-Bold.woff2', weight: '700', style: 'normal' }],
+  src: [{ path: './fonts/GT-America-Expanded-Bold.ttf', weight: '700', style: 'normal' }],
 });
